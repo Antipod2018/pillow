98bd4084c8772457d7e188e21feaecbfb73b4f75
Valentin_Mouse39 <47462272+Antipod2018@users.noreply.github.com>
2022-05-24 18:12:09 +0200

    добавил проверку всех директорий

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from PIL import Image, ImageDraw, ImageFont, ImageFilter, ImageOps
 import os, math
+from datetime import date, time, datetime
 
 
 def size_change(img_change):
@@ -39,14 +40,7 @@ def img_crop(img, a):
     img = img.crop((a,a,width-a,height-a))
     return  img
 
-if __name__ == '__main__':
-
-    with open("config.txt", 'r', encoding='utf-8') as f:
-        config = f.read()
-    f.close()
-    config = config.split('\n')
-    path = config[0]
-
+def in_folder(path):
     new_dir = os.path.join(path, os.path.basename(path))
     if not os.path.isdir(new_dir):
         os.mkdir(new_dir)
@@ -60,12 +54,10 @@ if __name__ == '__main__':
             new_img_path = os.path.join(new_dir, i)
             img1 = Image.open(path_img)
 
-
-
             im2 = Image.open('маска.jpg')  # маска для изображения
-            fill_color = 'black'
+
             im2 = ImageOps.invert(im2)  # если накладывается картинка на белом фоне, эту строку закоментить
-            img1 = mask(img1, im2)           #накладывает прозрачное изображение(изображение на черном фоне)
+            img1 = mask(img1, im2)  # накладывает прозрачное изображение(изображение на черном фоне)
 
             fill_color = 'black'
             if img1.mode in ('RGBA', 'LA'):  # это для нормальной работы маски
@@ -73,11 +65,49 @@ if __name__ == '__main__':
                 background.paste(img1, img1.split()[-1])
                 img1 = background
 
-            img1 = img_crop(img1, 10) #обрезает картинки, второй параметр - колл пикселей
-            img1 = img_smart_rotate(img1, 3) #вращает картинки на угол, указанный вторым параметром + обрезает черные углы
-            img1 = size_change(img1) #делает картинку 800Х600(второй параметр может быть меньше)
+            img1 = img_crop(img1, 10)  # обрезает картинки, второй параметр - колл пикселей
+            img1 = img_smart_rotate(img1,
+                                    3)  # вращает картинки на угол, указанный вторым параметром + обрезает черные углы
+            img1 = size_change(img1)  # делает картинку 800Х600(второй параметр может быть меньше)
 
             img1.save(new_img_path)
+
+
+def search_folder(path):
+    for lists in os.listdir(path):
+
+        in_right_dir = False
+        for file in os.listdir(path):
+            suffix = ('.png', 'jpg')
+            if file.endswith(suffix):
+
+                in_right_dir= True
+                break
+
+        if in_right_dir:
+            in_folder(path)
+            f = open('log.txt', 'a')
+            d = datetime.now()
+            d_string = d.strftime("     %d/%m/%Y   %H:%M:%S ")
+            print(os.path.basename(path), ' обработан ',d_string)
+            f.write(os.path.basename(path))
+            f.write(d_string)
+            f.write('\n')
+            f.close()
+            break
+        if not in_right_dir:
+            path1 = os.path.join(path, lists)
+            if os.path.isdir(path1): search_folder(path1)
+if __name__ == '__main__':
+
+    with open("config.txt", 'r', encoding='utf-8') as f:
+        config = f.read()
+    f.close()
+    config = config.split('\n')
+    path = config[0]
+
+    search_folder(path)
+
 
 
 
